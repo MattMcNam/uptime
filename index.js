115c2e3cd5056f6af010e1b57e9324e3b1384e82
Matthew McNamara <mattmcnam@gmail.com>
2016-05-10 00:58:55 +0200

    Get latest 50 broadcasts, add up times if the gap between broadcasts is under a threshold (hardcoded to 5 mins currently)

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ app.set('trust proxy', true);
 app.get('/channel/:channel', (req, res, next) => {
     const channel = req.params.channel;
     
-    request(`https://api.twitch.tv/kraken/channels/${channel}/videos?limit=1&broadcasts=true`, (err, resp, body) => {
+    request(`https://api.twitch.tv/kraken/channels/${channel}/videos?limit=50&broadcasts=true`, (err, resp, body) => {
         const data = JSON.parse(body);
         if (!data.videos || !data.videos[0] || data.videos[0].status !== 'recording') {
            res.send('Offline\n').end();
            return;
         }
         
-        const total = moment().diff(moment(data.videos[0]['recorded_at']), 'minutes');
+
+        const total = calculateDiff(data.videos, 5*60);
         const days = Math.floor(total/(60*24));
         const hours = Math.floor(total/60) - 24*days;
         const minutes = Math.floor(total % 60);
@@ -44,7 +45,27 @@ app.get('/channel/:channel', (req, res, next) => {
     });
 });
 
+function calculateDiff(videos, breakAllowance) {
+    breakAllowance = breakAllowance || 0;
+
+    let lastStart = moment(videos[0].recorded_at);
+    let total = moment().diff(lastStart, 'seconds');
+    for (let i = 1; i < videos.length; i++) {
+        let video = videos[i];
+        let vL = video.length;
+        let vS = moment(video.recorded_at);
+        let vE = vS.clone().add(vL, 'seconds');
+        if (lastStart.diff(vE, 'seconds') <= breakAllowance) {
+            total += vL;
+            lastStart = vS;
+        } else {
+            break;
+        }
+    }
+
+    return Math.floor(total/60);
+}
+
 app.use('/', express.static(`${__dirname}/static`));
 
 app.listen(9091, () => console.log('Listening on port 9091'));
-
